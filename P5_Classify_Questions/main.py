15ef750a3f76b470ef3c8f4baf2564fb2c108ae9
random-agile <61582258+random-agile@users.noreply.github.com>
2023-06-15 20:23:04 +0200

    Model API Tests

--- P5_Classify_Questions/main.py
+++ P5_Classify_Questions/main.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI
+from joblib import load
 
 app = FastAPI()
+
+# Charger le modèle à partir du fichier .pickle
+with open('logistic_regression_word2vec.pickle', 'rb') as f:
+    model = load(f)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
 
+
 @app.get("/predict/{sentence}")
 async def predict_sentence(sentence):
-# charger le modèle, lire une phrase et retourner le tag
-    print(sentence)
-    if len(sentence)%2==0:
-        tag = 'python'
-    else:
-        tag = 'java'
-    return {"tag": tag}
+    # Transformer la phrase en vecteur avec Word2Vec
+    vector = model.infer_vector(sentence.split())
+
+    # Utiliser le modèle pour prédire le tag
+    tag = model.predict([vector])[0]
+    return {"tags": tag}
