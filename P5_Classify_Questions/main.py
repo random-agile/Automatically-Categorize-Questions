7e073cbbf9f5cc1d1ef9558b23fdca782f36a25a
random-agile <o.miraciski@gmail.com>
2023-06-16 11:49:37 +0200

    Model Optimisation

--- P5_Classify_Questions/main.py
+++ P5_Classify_Questions/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from joblib import load
+import numpy as np
+import gensim
+from gensim.models import Word2Vec
 
 app = FastAPI()
+
+
+def get_word_embeddings(words, model_word2Vec):
+
+    embeddings = []
+    for word in words:
+        if word in model_word2Vec.wv.key_to_index:
+            embeddings.append(model_word2Vec.wv[word])
+    if not embeddings:
+        # Si aucun mot n'est présent dans le modèle Word2Vec, renvoyer un vecteur nul
+        embeddings.append(np.zeros(100))
+    return embeddings
+
 
 # Charger le modèle à partir du fichier .pickle
 with open('logistic_regression_word2vec.pickle', 'rb') as f:
@@ -18,11 +34,17 @@ async def read_item(item_id):
     return {"item_id": item_id}
 
 
+
 @app.get("/predict/{sentence}")
 async def predict_sentence(sentence):
+    sentence_list = [sentence]
+    model_word2Vec = Word2Vec(sentence_list, vector_size=100, window=5, min_count=1, workers=12)
     # Transformer la phrase en vecteur avec Word2Vec
-    vector = model.infer_vector(sentence.split())
-
+    sentence_embedding = [np.mean(get_word_embeddings(word, model_word2Vec), axis=0) for word in sentence_list]
+    print('sentence_embedding')
+    print(sentence_embedding)
     # Utiliser le modèle pour prédire le tag
-    tag = model.predict([vector])[0]
+    tag = model.predict(sentence_embedding)[0]
+    print(tag)
+    print('tag')
     return {"tags": tag}
