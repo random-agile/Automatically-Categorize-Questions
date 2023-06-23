8ea51af22886c93a64280b7c3d08c404ff371329
random-agile <o.miraciski@gmail.com>
2023-06-23 11:21:33 +0200

    Prediction with logistic regression and tfidf

--- P5_Classify_Questions/main.py
+++ P5_Classify_Questions/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from joblib import load
+import pickle
 import numpy as np
-import gensim
 from gensim.models import Word2Vec
 
 app = FastAPI()
 
 
-def get_word_embeddings(words, model_word2Vec):
-
+def get_word_embeddings(words, model_word2vec):
     embeddings = []
     for word in words:
-        if word in model_word2Vec.wv.key_to_index:
-            embeddings.append(model_word2Vec.wv[word])
+        if word in model_word2vec.wv.key_to_index:
+            embeddings.append(model_word2vec.wv[word])
     if not embeddings:
         # Si aucun mot n'est présent dans le modèle Word2Vec, renvoyer un vecteur nul
         embeddings.append(np.zeros(100))
@@ -22,6 +21,9 @@ def get_word_embeddings(words, model_word2Vec):
 # Charger le modèle à partir du fichier .pickle
 with open('logistic_regression_word2vec.pickle', 'rb') as f:
     model = load(f)
+
+with open('logistic_best.pickle', 'rb') as f:
+    model_logistic_vectorizer = pickle.load(f)
 
 
 @app.get("/")
@@ -34,13 +36,12 @@ async def read_item(item_id):
     return {"item_id": item_id}
 
 
-
-@app.get("/predict/{sentence}")
-async def predict_sentence(sentence):
+@app.get("/predict_word2vec/{sentence}")
+async def predict_sentence_word2vec(sentence):
     sentence_list = [sentence]
-    model_word2Vec = Word2Vec(sentence_list, vector_size=100, window=5, min_count=1, workers=12)
+    model_word2vec = Word2Vec(sentence_list, vector_size=100, window=5, min_count=1, workers=12)
     # Transformer la phrase en vecteur avec Word2Vec
-    sentence_embedding = [np.mean(get_word_embeddings(word, model_word2Vec), axis=0) for word in sentence_list]
+    sentence_embedding = [np.mean(get_word_embeddings(word, model_word2vec), axis=0) for word in sentence_list]
     print('sentence_embedding')
     print(sentence_embedding)
     # Utiliser le modèle pour prédire le tag
@@ -48,3 +49,12 @@ async def predict_sentence(sentence):
     print(tag)
     print('tag')
     return {"tags": tag}
+
+
+@app.get("/predict/{sentence}")
+async def predict_sentence(sentence):
+    # Exemple de prédiction avec une question donnée
+    sentence_list = [sentence]
+    predicted_keywords = model_logistic_vectorizer.predict(sentence_list)
+    print(f"Mots-clés prédits : {predicted_keywords}")
+    return {"tags": predicted_keywords[0]}
